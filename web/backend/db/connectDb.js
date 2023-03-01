41533536e60d783fd93e98242a039cc5b089b1d8
MD Abul Khayer <abul.k@aware.co.th>
2023-03-01 09:43:16 +0700

    2nd Update

---
+++ web/backend/db/connectDb.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+mongoose.set('strictQuery', false);
+
+// const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/Meetings";
+
+const connectDb = async () => {
+    try {
+        await mongoose.connect(MONGODB_URI);
+        console.log("Database connected Successfully");
+    } catch (error) {
+        console.log("there is some issues to connecting to DB", error);
+    }
+}
+module.exports = {connectDb};
